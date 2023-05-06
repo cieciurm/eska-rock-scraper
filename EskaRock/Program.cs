2c3d6c36c314698ae982c1121b2f95f9a4e70748
Mateusz Cieciura <mateusz.cieciura@gmail.com>
2023-05-06 18:31:32 +0200

    Add new results

--- EskaRock/Program.cs
+++ EskaRock/Program.cs
@@ -15,7 +15,7 @@ class Program
 
     static async Task Main(string[] args)
     {
-        var day = new DateTime(2023, 05, 01);
+        var day = new DateTime(2023, 05, 05);
 
         var reader = new SongReader();
         var songs = FromFile
